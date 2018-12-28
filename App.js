5b7a39d28dae0ccc7cc427e53cc1e140dadc7079
Wang Juntao <wangjt@zetyun.com>
2018-12-28 11:26:41 +0800

    #5

--- App.js
+++ App.js
@@ -4,20 +4,21 @@
  * @flow
  */
 
-import './shim.js';
-import 'crypto'
-import BottomTabBar from './BottomTabBar'
-import JPushModule from 'jpush-react-native';
-import JMessage from 'jmessage-react-plugin';
+import "./shim.js";
+import "crypto";
+import BottomTabBar from "./BottomTabBar";
+import JPushModule from "jpush-react-native";
+import JMessage from "jmessage-react-plugin";
 
 //var ws = new WebSocket('ws://120.78.72.237:3000');
 //var ws = new WebSocket('ws://192.168.71.42:3000');
 //import { PropTypes } from 'react';
 //import PropTypes from 'prop-types'
-var ws = new WebSocket('ws://192.168.1.4:3000');
+var ws = new WebSocket("ws://192.168.1.4:3000");
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
+  Alarm,
   Platform,
   StyleSheet,
   Text,
@@ -27,35 +28,35 @@ import {
   TextInput,
   Alert,
   ScrollView
-} from 'react-native';
-import Realm from 'realm';
+} from "react-native";
+import Realm from "realm";
 
 const WalletSchema = {
-  name: 'Wallet',
-  primaryKey: 'address',
+  name: "Wallet",
+  primaryKey: "address",
   properties: {
-    address: 'string',
-    name: 'string',
-    type: 'string',
-    keystore: 'string',//添加默认值的写法
+    address: "string",
+    name: "string",
+    type: "string",
+    keystore: "string" //添加默认值的写法
   }
 };
 JMessage.init({
   appkey: "1f1558cafbf2dd3946981f0b",
   isOpenMessageRoaming: false, // 是否开启消息漫游，默认不开启
-  isProduction: true, // 是否为生产模式
-})
-JMessage.setDebugMode({ enable: true })
-JMessage.setBadge(5, (success) => {})
+  isProduction: true // 是否为生产模式
+});
+JMessage.setDebugMode({ enable: true });
+JMessage.setBadge(5, success => {});
 
-let realm = new Realm({schema: [WalletSchema]});
+let realm = new Realm({ schema: [WalletSchema] });
 //alert(PropTypes.string)
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={{flex:1, justifyContent: 'flex-end'}}>
-        <BottomTabBar ws={ws} WalletSchema={WalletSchema}></BottomTabBar>
+      <View style={{ flex: 1, justifyContent: "flex-end" }}>
+        <BottomTabBar ws={ws} WalletSchema={WalletSchema} />
       </View>
     );
   }
@@ -64,18 +65,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
