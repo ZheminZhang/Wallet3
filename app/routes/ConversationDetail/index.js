cb8a979a81ee36455b7190e041d6685f25d362e5
Wang Juntao <wangjt@zetyun.com>
2019-01-02 14:22:14 +0800

    #14

--- app/routes/ConversationDetail/index.js
+++ app/routes/ConversationDetail/index.js
@@ -1,76 +1,64 @@
-'use strict';
+"use strict";
 
-import React from 'react';
-import ReactNative from 'react-native';
-import JMessage from 'jmessage-react-plugin';
-import {
-	TabNavigator
-} from 'react-navigation';
-
+import React from "react";
+import ReactNative from "react-native";
+import JMessage from "jmessage-react-plugin";
+import { TabNavigator } from "react-navigation";
 
 const {
-    View,
-    Text,
-    TouchableHighlight,
-    StyleSheet,
-    Button,
-    Alert,
-    TextInput,
-    Image,
-    FlatList,
-    ScrollView,
-  } = ReactNative;
-
-
+  View,
+  Text,
+  TouchableHighlight,
+  StyleSheet,
+  Button,
+  Alert,
+  TextInput,
+  Image,
+  FlatList,
+  ScrollView
+} = ReactNative;
 
 export default class ConversationDetail extends React.Component {
-    static navigationOptions = {
-      title: "会话详情",
+  static navigationOptions = {
+    headerTitle: "会话详情"
+  };
+
+  static navigationOptions = ({ navigation }) => {
+    const { params = {} } = navigation.state;
+    return {
+      headerLeft: (
+        <Button
+          title="返回"
+          onPress={() => {
+            // this.props.navigation.goBack()
+            navigation.goBack();
+            // this.props.navigation.goBack()
+            // this.onGoBack()
+            // Alert.alert("navigation", this.)
+            setTimeout(() => {
+              params.onGoBack();
+            }, 100);
+
+            // Alert.alert("goback",par)
+          }}
+        />
+      ),
+      title: "会话详情"
     };
+  };
 
-    static navigationOptions = ({
-      navigation
-    }) => {
-        const {
-            params = {}
-        } = navigation.state;
-        return {
-            headerLeft: <Button title="返回" onPress={() => { 
-              // this.props.navigation.goBack()
-              navigation.goBack()
-              // this.props.navigation.goBack() 
-              // this.onGoBack()
-              // Alert.alert("navigation", this.)
-              setTimeout(() => {
-                params.onGoBack()
-              }, 100); 
-              
-              // Alert.alert("goback",par)
-            }} />,
-            title: "会话详情",
-    
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
     };
+    // this.props.navigation.state.params.onGoBack();
+    // Alert.alert("navigation", JSON.stringify(this.props.navigation.state.params.onGoBack))
+  }
 
-    constructor(props) {
-      super(props)
-      this.state = {
-        data: [],
-      } 
-      // this.props.navigation.state.params.onGoBack();
-      // Alert.alert("navigation", JSON.stringify(this.props.navigation.state.params.onGoBack))
-    }
+  componentWillMount() {}
 
-    componentWillMount() {
-    }
-
-    
-    render() {
-
-      return (
-        <ScrollView>
-            
-        </ScrollView>
-      );
-    }
+  render() {
+    return <ScrollView />;
+  }
 }
