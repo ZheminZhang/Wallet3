4ed7f9eb58eb785ee01009379fe754fda59ca69b
Wang Juntao <wangjt@zetyun.com>
2019-04-16 14:17:08 +0800

    最新版

--- App.js
+++ App.js
@@ -3,32 +3,22 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-import './shim.js';
-import 'crypto'
-import BottomTabBar from './BottomTabBar'
-import JPushModule from 'jpush-react-native';
-import JMessage from 'jmessage-react-plugin';
-import Chat from './lib/ChatComponent'
-import httpRequest from './lib/src/httpRequest'
+import "./shim.js";
+import "crypto";
+import BottomTabBar from "./BottomTabBar";
+import JPushModule from "jpush-react-native";
+import JMessage from "jmessage-react-plugin";
+import Chat from "./lib/ChatComponent";
+import httpRequest from "./lib/src/httpRequest";
 
 //var ws = new WebSocket('ws://120.78.72.237:3000');
 //var ws = new WebSocket('ws://192.168.71.42:3000');
 //import { PropTypes } from 'react';
 //import PropTypes from 'prop-types'
-var ws = new WebSocket("ws://192.168.1.4:3000");
+var ws = new WebSocket("ws://192.168.0.104:3000");
 
 import React, { Component } from "react";
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  AlertIOS,
-  TextInput,
-  Alert,
-  ScrollView
-} from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import Realm from "realm";
 
 const WalletSchema = {
@@ -43,7 +33,7 @@ const WalletSchema = {
   }
 };
 JMessage.init({
-  appkey: "1f1558cafbf2dd3946981f0b",
+  appkey: "4dc08da5773df4784e216e5c",
   isOpenMessageRoaming: false, // 是否开启消息漫游，默认不开启
   isProduction: true // 是否为生产模式
 });
@@ -57,27 +47,12 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "flex-end" }}>
-        <BottomTabBar httpRequest = {httpRequest} ws={ws} WalletSchema={WalletSchema} />
+        <BottomTabBar
+          httpRequest={httpRequest}
+          ws={ws}
+          WalletSchema={WalletSchema}
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
